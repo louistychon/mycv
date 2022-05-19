df430c2e0c8bc603103a40db3d9ee1e756afb0aa
louis.tychon1@gmail.com <louis.tychon1@gmail.com>
2022-05-19 16:17:01 +0200

    contact form(sends data to mysql db)

---
+++ contact.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link rel="stylesheet" href="cv.css">
+    <title>Contactez-moi</title>
+
+</head>
+
+<body>
+<h1 class="text-center display-1">Contact me</h1>
+    <div class="form-container container text-center mt-5">
+        <form method="post" action="postvalidation.php" class="d-flex-col">
+            <div class="input mt-1"><input type="text" name="nom" class="rounded-pill" placeholder="Your name*" required></div>
+            <div class="input mt-1"><input  class="rounded-pill" type="url" name="job" placeholder="Job link"></div>
+            <div class="input mt-1"><input class="rounded-pill" type="text" name="email" placeholder="E-mail*" required></div>
+            <div class="input mt-1"><input class="rounded-pill" type="text" name="msg" placeholder="Your message" required></div>
+            <button type="submit" class="rounded-pill btn btn-primary mt-1">Send form</button>
+        </form>
+
+        <div> Note : votre e-mail est entièrement chiffré dans notre base de données (c'est-à-dire illisible pour d'éventuels hackers)<br>le Hash de votre e-mail : </div>
+    </div>
+
+
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+    <script src="https://kit.fontawesome.com/0f04e7ed22.js" crossorigin="anonymous"></script>
+</body>
+
+</html>
--- index.php
+++ index.php
@@ -135,6 +135,7 @@
             </ul>
         </div>
       </div>
+        <a href="/contact.php">contactez-moi</a>
   </main>
   <?php include("footer.php"); ?>
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
